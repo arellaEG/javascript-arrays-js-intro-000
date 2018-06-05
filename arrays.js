37db35aaee055e8719ddaaff1a7b0ff9a71f4e96
Arella Gussow <arellaeg@gmail.com>
2018-06-05 07:11:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,7 @@ function destructivelyAddElementToBeginningOfArray (array, element){
   return array
 }
 
-function addElementEndOfArray (array, element){
+function addElementToEndOfArray (array, element){
   return [...array, element]
 }
 
